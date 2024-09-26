2b3a1d76320b9ce24756474be00ac2caec3c368b
yadagiritalada143 <ramyathummala10@gmail.com>
2024-09-27 00:35:52 +0530

    Implement a new API to fetch themes for an Organization.

--- src/controllers/common/getOrganizationThemesController.ts
+++ src/controllers/common/getOrganizationThemesController.ts
@@ -5,9 +5,10 @@ import { ORGANIZATION_THEMES_ERROR_MESSAGES } from '../../constants/commonErrorM
 const getOrgazationThemes = (req: Request, res: Response) => {
     console.log('Request Params are:', req.params);
     const { organization_name } = req.params;
-    getOrganizationThemesService.getOrgThemes(organization_name)
+    getOrganizationThemesService
+        .getOrgThemes(organization_name)
         .then((themesResponse: any) => {
-
+            res.status(200).json({OrgazationThemes : themesResponse });
         })
         .catch((error: any) => {
             console.error(error);
--- src/model/organizationThemesModel.ts
+++ src/model/organizationThemesModel.ts
@@ -8,7 +8,21 @@ const OrganizationThemesSchema = new mongoose.Schema({
         organization: { type: mongoose.Schema.Types.String },
         logo: { type: mongoose.Schema.Types.String },
         theme: {
-
+            primaryColor : { type: mongoose.Schema.Types.String },
+            colorScheme : { type: mongoose.Schema.Types.String },
+            fontFamily : { type: mongoose.Schema.Types.String },
+            button :{
+                color : { type: mongoose.Schema.Types.String },
+                textColor : { type: mongoose.Schema.Types.String },
+            },
+            colors :{
+                primary : [{ type: mongoose.Schema.Types.Array }],
+                secondary : [{ type: mongoose.Schema.Types.Array }]
+            },
+            color : { type: mongoose.Schema.Types.String },
+            backgroundColor : { type: mongoose.Schema.Types.String },
+            borderColor : { type: mongoose.Schema.Types.String },
+            headerBackgroundColor : { type: mongoose.Schema.Types.String },
         }
     }
 }, {
