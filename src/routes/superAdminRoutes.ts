7c1074974bed24531beb8452e2e635fd598f0292
YadagiriTalada <150346563+yadagiritalada143@users.noreply.github.com>
2024-11-20 12:27:28 +0530

    Delete src/routes/superAdminRoutes.ts

--- src/routes/superAdminRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router } from 'express';
-import getAllEmployeesBySuperAdminController from '../controllers/superAdmin/getAllEmployeesBySuperAdminController';
-
-const superAdminRouter: Router = express.Router();
-superAdminRouter.get('/getAllEmployeesBySuperAdmin', getAllEmployeesBySuperAdminController.getAllEmployeesBySuperAdmin);
-
-export default superAdminRouter;
